e7eb2b1a076b6cda1368cae159d5717aaf266f11
vasir <vasirsensei@gmail.com>
2026-07-15 02:19:34 +0300

    Update JDAForgePlus.java

--- src/main/java/me/vasir/forgeplus/core/JDAForgePlus.java
+++ src/main/java/me/vasir/forgeplus/core/JDAForgePlus.java
@@ -46,7 +46,7 @@ public class JDAForgePlus extends ForgeModule {
             logRetentionTask.start();
         }
 
-        Log.info("enabling " + name() + " " + version());
+        Log.info("Enabling " + name() + " " + version());
     }
 
     @Override
